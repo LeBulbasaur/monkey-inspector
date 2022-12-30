d07cf7b32c1a5e5b3b9b5bc0ca12b5ef7e1293e2
Antoni Zaremba <antek.zaremba@gmail.com>
2022-12-30 13:21:31 +0100

    changes

--- client/src/pages/Inspect/Inspect.tsx
+++ client/src/pages/Inspect/Inspect.tsx
@@ -1,15 +1,46 @@
-import { useState } from "react";
 import "./Inspect.css";
+import * as THREE from "three";
+import React, { useRef, useState, Suspense } from "react";
+import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
+import Earth from "../../models/Earth";
 
 type Props = {
   image: File | undefined;
   setImage: React.Dispatch<React.SetStateAction<File | undefined>>;
 };
 
+function Box(props: ThreeElements["mesh"]) {
+  const mesh = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => (mesh.current.rotation.x += delta));
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
 function Inspect(props: Props) {
   return (
     <div className="Inspect">
       <h1>Inspekcja sanitarna</h1>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Suspense fallback={null}>
+          {/* <Earth position={[0, 0, 0]} /> */}
+        </Suspense>
+      </Canvas>
+      ,
     </div>
   );
 }
